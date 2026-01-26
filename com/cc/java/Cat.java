51ddd6a308939f694c432d2a5eb1f221d3b86bed
jy4education_git <jy4education@gmail.com>
2026-01-26 08:49:26 +0100

    +

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -3,6 +3,7 @@ package com.cc.java;
 public class Cat {
 
     public void meow() {
-        System.out.println("Meow!!!");
+        // System.out.println("Meow!!!");
+        System.out.println(this); // Ausgabe der Referenzadresse im Speicher
     }
 }
