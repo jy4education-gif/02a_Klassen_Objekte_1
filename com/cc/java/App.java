93ba983f69c37eae2ce749fbeea489d84d8361fd
jy4education_git <jy4education@gmail.com>
2026-02-02 15:36:59 +0100

    +

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -17,13 +17,13 @@ public class App {
         Cat cat = new Cat();  // default - Konstruktor
         output("Blick von aussen auf cat: " + cat);  // Ausgabe der Referenzadresse im Speicher
         output("Blick von innen auf cat: "+ cat.getInstanceVariable());
-        // cat.meow(); // Aufruf der meow-Methode
+        cat.meow(); // Aufruf der meow-Methode
         output("-----------------------");
        
         Cat cat2 = new Cat();  // default - Konstruktor
         output("Blick von aussen auf cat2: " + cat2);  // Ausgabe der Referenzadresse im Speicher
         output("Blick von innen auf cat2: "+ cat2.getInstanceVariable());
-        // cat2.meow(); // Aufruf der meow-Methode
+        cat2.meow(); // Aufruf der meow-Methode
         output("-----------------------");
    
     }
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -3,7 +3,7 @@ package com.cc.java;
 public class Cat {
 
     public void meow() {
-        // System.out.println("Meow!!!");
+        System.out.println("Meow!!!");
         // Instanzvariable this referenziert das aktuelle Objekt
         //System.out.println("Blick von innen auf cat: " + this); // Ausgabe der Referenzadresse im Speicher
         App.output("Blick von innen auf cat: " + this);
